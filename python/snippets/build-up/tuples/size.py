2a58af5941ae137243fa4bdab666ffd66a3fa37b
gioele-bernardini <gioele.g@outlook.com>
2024-04-01 12:37:58 +0200

    .

---
+++ python/snippets/build-up/tuples/size.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import sys
+
+tuple1 = ("A", 1, "B", "C", 3)
+tuple2 = ("Ciao", "sono", "Gioele")
+
+print("Size of tuple1: " + str(sys.getsizeof(tuple1)) + "bytes")
+print('Len of tuple1: ', len(tuple1))
