87e4999fac34ad4ceb0681674f371e5b7ac50748
gioele-bernardini <gioele.g@outlook.com>
2024-04-03 12:49:28 +0200

    .

---
+++ python/snippets/build-up/tuples/size.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+  printf("Dimensione di un intero in C => %ld (byte)\n", sizeof(int));
+
+  return 0;
+}
--- python/snippets/build-up/tuples/size.py
+++ python/snippets/build-up/tuples/size.py
@@ -2,8 +2,21 @@
 
 import sys
 
-tuple1 = ("A", 1, "B", "C", 3)
-tuple2 = ("Ciao", "sono", "Gioele")
+# tuple1 = ("A", 1, "B", "C", 3)
+# tuple2 = ("Ciao", "sono", "Gioele")
 
-print("Size of tuple1: " + str(sys.getsizeof(tuple1)) + "bytes")
-print('Len of tuple1: ', len(tuple1))
+# print("Size of tuple1: " + str(sys.getsizeof(tuple1)) + "bytes")
+# print('Len of tuple1: ', len(tuple1))
+
+print('Dimensione di un intero in python => ' + str(sys.getsizeof(1)) + ' (byte)')
+
+
+
+
+# #include <stdio.h>
+
+# int main() {
+#   printf("Dimensione di un intero in C => %ld (byte)\n", sizeof(int));
+
+#   return 0;
+# }
