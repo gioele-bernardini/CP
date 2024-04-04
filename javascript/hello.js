78995d26aec74ece1c7e1dd48c0c448157e6e1d6
gioele-bernardini <gioele.g@outlook.com>
2024-04-04 11:11:05 +0200

    .

---
+++ javascript/hello.js
@@ -0,0 +1,15 @@
+// This file covers the basics of the first lecture
+
+console.log('Hello, world!');
+
+var myVar;
+console.log(typeof (myVar));
+
+myVar = 'Pippo';
+myVar = 5;
+myVar = true;
+myVar = [1, 2, 3];
+myVar = {key1: 'value1'};
+myVar = null;
+
+myVar = function(n) { return n + 1 };
--- python/roulette/chat.py
+++ python/roulette/chat.py
@@ -20,7 +20,7 @@ def bye_bye():
   message = '[headless is OK]'
   # INIZIALIZZA CONNESSIONE!!!
   chrome_options = Options()
-  chrome_options.add_argument("--headless")
+  # chrome_options.add_argument("--headless")
 
   driver = webdriver.Chrome(options=chrome_options)
 
