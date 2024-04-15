3087b2f02fcde69acf67f3e0394fb441f3fcd500
gioele-bernardini <gioele.g@outlook.com>
2024-04-15 22:24:53 +0200

    .

--- javascript/pong/pong.js
+++ javascript/pong/pong.js
@@ -7,12 +7,22 @@ let context;
 // Players
 let playerWidth = 10;
 let playerHeight = 50;
+let playerVelocityY = 0;
 
-let player = {
-  x: 10,
-  y: boardHeight / 2,
-  width: playerWidth,
-  height: playerHeight,
+let player1 = {
+  x  : 10,
+  y : boardHeight / 2,
+  width : playerWidth,
+  height : playerHeight,
+  velocityY : playerVelocityY,
+}
+
+let player2 = {
+  x : boardWidth - playerWidth - 10,
+  y : boardHeight / 2,
+  width : playerWidth,
+  height : playerHeight,
+  velocityY : playerVelocityY,
 }
 
 window.onload = function() {
@@ -20,4 +30,56 @@ window.onload = function() {
   board.height = boardHeight;
   board.width = boardWidth;
   context = board.getContext('2d'); // Used for drawing
+
+  // Draw initial player1
+  context.fillStyle = '#FBDB65';
+  context.fillRect(player1.x, player1.y, player1.width, player1.height);
+
+  requestAnimationFrame(update);
+  document.addEventListener('keyup', movePlayer);
+}
+
+function update() {
+  requestAnimationFrame(update);
+  context.clearRect(0, 0, board.width, board.height);
+
+  // Player1
+  context.fillStyle = '#FBDB65';
+  // player1.y += player1.velocityY;
+  let nextPlayer1Y = player1.y + player1.velocityY;
+  if (!outOfBounds(nextPlayer1Y)) {
+    player1.y = nextPlayer1Y;
+  }
+
+  context.fillRect(player1.x, player1.y, player1.width, player1.height);
+
+  // Player2
+  // player2.y += player2.velocityY;
+  let nextPlayer2Y = player2.y + player2.velocityY;
+  if (!outOfBounds(nextPlayer2Y)) {
+    player2.y = nextPlayer2Y;
+  }
+  context.fillRect(player2.x, player2.y, player2.width, player2.height);
+}
+
+function outOfBounds(yPosition) {
+  return (yPosition < 0 || yPosition + playerHeight > boardHeight);
+}
+
+function movePlayer(e) {
+    // Player1
+  if (e.code == 'KeyW') {
+    player1.velocityY = -3;
+  }
+  else if (e.code == 'KeyS') {
+    player1.velocityY = 3;
+  }
+
+  // Player 2
+  if (e.code == 'ArrowUp') {
+    player2.velocityY = -3;
+  }
+  else if (e.code == 'ArrowDown') {
+    player2.velocityY = 3;
+  }
 }
