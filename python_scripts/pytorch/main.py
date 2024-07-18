749eafc6ce9702f9dd83934717ddba1d7f9a6d8d
gioele-bernardini <gioele.g@outlook.com>
2024-07-19 00:05:33 +0200

    Add: added logic for splitting between validation and training set.

--- python_scripts/pytorch/main.py
+++ python_scripts/pytorch/main.py
@@ -1,6 +1,9 @@
+import numpy as np
+import torch
+from torchvision import datasets, transforms
+
 # Define the number of subprocesses to use for data loading,
 # the batch size, and validation size
-
 num_workers = 0
 batch_size = 0
 valid_size = 0
@@ -16,10 +19,18 @@ train_data = datasets.MNIST(
   transform = transform,
 )
 
-
-train_data = datasets.MNIST(
+test_data = datasets.MNIST(
   root = 'data',
   train = False,
   download = True,
   transform = transform,
 )
+
+num_train = len(train_data)
+indices = list(range(num_train))
+np.random_shuffle(indices)
+
+split = int(np.floor(valid_size * num_train))
+train_idx, valid_idx = indices[split:], indices[:split]
+
+
