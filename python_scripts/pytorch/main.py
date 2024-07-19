641b47752d7d560dbb658c2eb53e3cedb34113b7
gioele-bernardini <gioele.g@outlook.com>
2024-07-19 09:39:45 +0200

    Add: forward added to the neural network.

--- python_scripts/pytorch/main.py
+++ python_scripts/pytorch/main.py
@@ -1,36 +1,90 @@
 import numpy as np
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from torchvision import datasets, transforms
 
 # Define the number of subprocesses to use for data loading,
 # the batch size, and validation size
 num_workers = 0
-batch_size = 0
-valid_size = 0
+batch_size = 64  # Set a realistic batch size
+valid_size = 0.2  # Set a realistic validation size
 
 # Convert to float tensor
 transform = transforms.ToTensor()
 
 # Choose the training and test datasets
 train_data = datasets.MNIST(
-  root = 'data',
-  train = True,
-  download = True,
-  transform = transform,
+  root='data',
+  train=True,
+  download=True,
+  transform=transform,
 )
 
 test_data = datasets.MNIST(
-  root = 'data',
-  train = False,
-  download = True,
-  transform = transform,
+  root='data',
+  train=False,
+  download=True,
+  transform=transform,
 )
 
 num_train = len(train_data)
 indices = list(range(num_train))
-np.random_shuffle(indices)
+np.random.shuffle(indices)
 
 split = int(np.floor(valid_size * num_train))
 train_idx, valid_idx = indices[split:], indices[:split]
 
+train_sampler = torch.utils.data.SubsetRandomSampler(train_idx)
+valid_sampler = torch.utils.data.SubsetRandomSampler(valid_idx)
+
+train_loader = torch.utils.data.DataLoader(
+  train_data,
+  batch_size=batch_size,
+  sampler=train_sampler,
+  num_workers=num_workers,
+)
+
+valid_loader = torch.utils.data.DataLoader(
+  train_data,
+  batch_size=batch_size,
+  sampler=valid_sampler,
+  num_workers=num_workers,
+)
+
+test_loader = torch.utils.data.DataLoader(
+  test_data,
+  batch_size=batch_size,
+  num_workers=num_workers,
+)
+
+# We then simply obtain one batch of images for training using iter
+dataiter = iter(train_loader)
+images, labels = dataiter.next()
+images = images.numpy()
+
+class Net(nn.Module):
+  def __init__(self):
+    super(Net, self).__init__()
+    hidden_1 = 512
+    hidden_2 = 512
+    self.fc1 = nn.Linear(28 * 28, hidden_1)
+    self.fc2 = nn.Linear(hidden_1, hidden_2)
+    self.fc3 = nn.Linear(hidden_2, 10)
+    self.dropout = nn.Dropout(0.5)
+
+  def forward(self, x):
+    # flatten image input
+    x = x.view(-1, 28 * 28)
+    # add hidden layer, with relu activation function
+    x = F.relu(self.fc1(x))
+    # add dropout layer
+    x = self.dropout(x)
+    # add hidden layer, with relu activation function
+    x = F.relu(self.fc2(x))
+    # add dropout layer
+    x = self.dropout(x)
+    # add output layer
+    x = self.fc3(x)
+    return x
 
