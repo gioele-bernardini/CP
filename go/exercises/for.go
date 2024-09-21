ee031c5da78f19c7ba3cfcc717e096e17435dcb3
gioele-bernardini <gioele.g@outlook.com>
2024-09-21 16:11:30 +0200

    Add: complete first few exercises for golang series.

---
+++ go/exercises/for.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i = i + 1
+	}
+
+	for i := range 3 {
+		fmt.Println("range", i)
+	}
+
+	for {
+		fmt.Println("loop")
+		break
+	}
+
+	for n := range 6 {
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
+}
